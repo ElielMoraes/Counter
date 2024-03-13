e6a7975d9599d44b0af1c373dbf7ba8cfa30b37a
ElielMoraes <eliel.moraes.martins24@gmail.com>
2024-03-13 16:22:25 -0300

    oi

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 
+let initialValue = 0;
+let currentValue;
+let value = 0;
+let first = true;
 
-const PI = 3.14159;
-let radius;
-let circumference;
+document.getElementById("increaseButton").onclick = function(){
+    // if(first = true){
+    //     value = initialValue++;
+    //     currentValue=value;
+    //     first = false;
+    // }else{
+    //     value = currentValue++;
+    //     currentValue= value;
+    // }
+    value++;
 
-document.getElementById("mySubmit").value = function(){
-    radius = document.getElementById("myText");
-    circumference = 2 * PI * radius;
-    circumference = String(circumference);
-    document.getElementById("myP").textContent = `Result = ${circumference}`;
+    document.getElementById("countLabel").textContent = value;
 }
 
+document.getElementById("decreaseButton").onclick = function(){
+    // if(first = true){
+    //     value = initialValue--;
+    //     currentValue=value;
+    //     first = false;
+    // }else{
+    //     value = currentValue--;
+    //     currentValue= value;
+    // }
 
 
-// circumference = 2 * PI * radius;
+    value--;
 
+    document.getElementById("countLabel").textContent = value;
+}
+
+document.getElementById("resetButton").onclick = function(){
+    // initialValue = 0
+    // currentValue = 0;
+    value = 0;
+    document.getElementById("countLabel").textContent = value;
+}
